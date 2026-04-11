55f4b87bada249c64be0369606a2345aedbddc40
Liliiiin <janonaturgunova029@gmail.com>
2026-04-11 22:10:03 +0900

    Add files via upload

---
+++ matn string 4 dars.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 08:31:43 2026
+
+@author: ASUS
+"""
+
+#xabar=' men bugun antiqa harfni unicod table.comda kochirib\
+    #oldin un kod hozir "Ϊ" manashu kod'
+#print(xabar)
+#xitoy_tili= "antiqa narsalar bor xitoy tilida for example '勘' shuyam harf\
+#mi?"
+#print(xitoy_tili)
+mano= 'men ozim hamma mano'
+smile='🤣'
+
+yana_jinnichasidan='😝'
+
+#ism= "Laziz"
+#print("Mening ismim"+  ism) bu + bilan matnlarni bir biriga boglahs mumkin
+#ekan.
+ism='Ahad'
+familiya='Olimov'
+#print(ism+ ' ' +familiya) bu yerda ikki qavs ochilisgin9 sababi soz orasida boshliq
+#hosil qilish uchun masofa tashlashda ishlatilgan
+#print(ism+familiya)
+  
+#ism='Lobar'
+#familiya='Jamolova'
+#name_surname= f"{ism} {familiya}" bu yerda ikkita string jamlanagan 3 chi string 
+#print(name_surname) F string yordamida bir neha sozlarni bir biriga boglash mumkin
+# bunda f string qoyib qavsni ichida {} shunda qavs ochib unga matnni yuklanadi.
+ 
+#ism='Maykl'
+#familiya= 'Taysn'
+#print(f"Hello,Mening ismim {familiya}. {ism} {familiya}")
+
+#print('hello world')
+#print('Hello\t World') bu yerda \t uzun boshliq qoldiradi matnlar orasida
+#print('Hello\n WOrld') bu eSA OLDIngi darslardagi qator kochirish
+
+# String metodlar
+#ism=" brus"
+#familiya=' li'
+#ism_surname=f"{ism} {familiya}" 
+#print(ism_surname.upper()) shu upper metodi bilan matn
+#harflarini katta harflarga otqazish mumkin
+
+#familiya='jek'
+#ism_surname= f"{ism} {familiya}"
+#ism_surname= ism_surname.upper() bu yerda faylni ichidagi matnni katta harfga otqazishda ishlatilgan
+#print(ism_surname)
+
+#ism= "Bernard"
+#familiya="Lipon"
+#name_surname=f" {ism} {familiya}"
+#print(name_surname.lower()) bu yerda lower() metodi katta harflarni kichik harfga
+#otqazib beradi
+ 
+
+ism='anish'
+familiya='giri'
+ism_familiya=f"{ism} {familiya}"
+#print(ism_familiya.title()) bu metodda Soz boshidagi harfni harfi katta 
+#bilan yoziladi
+#print(ism_familiya.capitalize()) capitalazee () metdi bilan sozni 
+#boshini bosh harf bilan boshlash mumkin
+
+meva='        anjir       '
+#print("Men" +  meva. lstrip() + "ni yaxshi koraman") #chap tomonga surib qoyadi matni
+ 
+#print("Men" + meva.rstrip() + "ni yomon koraman") # bu metod ong tomondagi 
+#boshliqni olib tashlaydi
+#print("dugonamni"  +meva.strip()+  "ga allergiyasi bor")
+
+# Input funksiyaai fu foydalanuvchidan biror narsani soraganda ishlatiladi.
+#inputni matnni yonidan qoshamiz
+ 
+
+#ism=input("whats your name?:")
+#print("Hello ", +ism.upper())
+
+#ism=input("isming nima?")
+#print("Hi", +ism)
+
+#ism=input("Ismingiz nima?\n>>>")
+#print("Assalomu alaykum",ism.title())
+
+
