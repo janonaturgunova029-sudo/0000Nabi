ce2b6128a9d2a4786b6ca1bee8bc98a0c829430d
Liliiiin <janonaturgunova029@gmail.com>
2026-04-11 22:09:11 +0900

    Add files via upload

---
+++ 5 mavzu son vazifa.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  5 17:35:04 2026
+
+@author: ASUS
+"""
+
+#x=int(input("Istalgan sonni kiriting?\n>>>"))
+#istalga_son=n
+#sonning_kvadrati=(son**2)
+#print(x,"ning kvadrati", x**2,"ga teng")
+#print(x, "ning kubi esa", x**3, "ga teng")
+#print(x, 'ning to\'rtinchi darajasi esa', x**4, "ga teng")
+
+
+
+#t_yil=int(input("Tugilgan yilingizni kiriting?\n>>"))
+#yosh=(2026-t_yil)
+#print("siz", yosh, "yoshda ekansiz!")
+
+
+#a=float(input("Istalgan birinchi sonni kiriting:\n>>"))
+#b=float(input("Istalgan ikkinchi sonni kiriting!\n>>"))
+#print(f"{a}+{b}=", a+b)
+#print(f"{a}-{b}=", a-b)
+#print(f"{a}/{b}=", a/b)
+#print(f"{a}*{b}=", a*b)
+   
+
+#!print("Hello world")
+
+
+#xabar= ("men bir kun amerikaga borib trampni uchrataman")
+#print(xabar)
+
+#radiusi=38
+#PI=3.1415
+#aylana_yuzi=PI*radiusi**2
+#print("radiusi", radiusi, "ga teng aylananing yuzi=", aylana_yuzi)
+
+
+# 2 dars darslari
+ism='Rano'
+print("qoshni qizni ismi" +' '+ism)
+
+
+ism='Maykl'
+print("Bir idiot odamni ismi bor u:" +ism.rstrip())
+
+
+ism="ian"
+familiya="samuel"
+#print(ism.upper(), familiya.upper())
+shaxs= f"{ism} {familiya}"
+#print(shaxs.title())
+print(shaxs.capitalize())
+
+ism="lili"
+familiya="kim"
+print(ism+ '  ' + familiya. title())
+ 
+name= 'sem'
+surname=' len'
+#keng=(name+surname.upper())
+print(f"Salom mening ismim {name}.{name} {surname}")
+
+ism=input('Ismingiz nima?:')
+print("Salom mening ismim", ism.title())
+
+ism=input("Isminngizni kiriting?\n>>")
+print("Salom, siz bilan tanishganmdan juda hursandman", ism.upper())
