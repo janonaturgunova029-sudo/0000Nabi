9b5be7efe1aa37c1431f9b2ece675261ab9be20a
Liliiiin <janonaturgunova029@gmail.com>
2026-04-19 20:43:35 +0900

    Add files via upload

---
+++ fot_tsikli 9 py.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 22:04:34 2026
+
+@author: ASUS
+"""
+
+#sonlar=list(range(0,21))
+#sonlar_kvadrati=[]
+#for son in sonlar:
+ #   sonlar_kvadrati.append(son**2)
+    
+#print(sonlar) 
+#print(sonlar_kvadrati)  
+
+
+#friends=[]
+#print("5 ta eng yaqin dostingizni kiriting")
+
+#for n in range(5):
+#    friends.append(input(f"{n+1}-dostingizni ismini kiriting?"))
+#print(friends)
+ 
+
+#gullar=[]
+#print("Eng yoqtirgan 6 ta gul nomini kiriting:")
+
+#for gul in range(6):
+#    gullar.append(input(f"{gul+1}-gulni nomini kiriting!:"))
+    
+#print(gullar)    
+
+#손님=['alisher', 'olim','Sardor','Komil']
+#for mehmon in 손님:
+   # print(f"Hurmatli {mehmon}, 손님을 4 월 5 일날 결혼식에 초대합니다")
+   # print("와주시면 너무 간사하겠습니다")
+    
+  
+#손님=['alisher', 'olim','Sardor','Komil']    
+#for mehmon in 손님:
+#    print(mehmon)
+    
+#print(손님)    
+
+#숫자=[2,9,3,10,22,31,12]
+#for raqam in 숫자:
+    #X print(f"agar raqamingiz {숫자}- bolsa bugun doktor qabuliga keling")    
+#print(숫자)    
+    
+#sonlar=list(range(1,11))
+#for son in sonlar:
+   # print(f" {son} ning kvadrati{son**2} ga teng")
+ 
+#ismlar=['ali',"barno",'lobar','akmal','shavkat']
+#for n in ismlar:
+#
+#print(f"Kod{len(ismlar)} marta takrorlandi")
+
+#sonlar=list(range(10,100 ,2))
+#for son in sonlar:
+   # sonlar=[]
+   #print(son**3) #SONni kubni chiqarish
+   
+#sonlar=list(range(10,80,2))
+#for son in sonlar:
+ #   print(son)   
+ 
+sonlar=list(range(10,70,2))
+for n in sonlar:
+   print(n**2) 
+ 
+#filmlar=[]
+#for film in filmlar:
+#print("5 ta Eng yoqtirgar filmizni kiriting ")
+#for k in range(5):
+#    filmlar.append(input(f"{k+1}-kino:"))
+#print(filmlar)   
+
+
+#gullar=[]
+#for g in gullar:
+#print("3 ta eng yoqtirgan gul nomi:")
+#for g in range(3):
+ #   gullar.append(input(f"{g+1}-gul:"))
+#    print(gullar)
+    
+#n_odam=int(input("Bugun nechta odam bilan korishdiz?:"))
+#ismlar=[]
+#for n in range(n_odam):
+    #ismlar.append(input(f" {n+1}-suhbat qilgan odamizni kiriting"))
+    #print(ismlar)
+    
+    
+n_bolalar=int(input("bugun nechta bola bilan oynadiz?:"))
+ismlar=[]
+for n in range(n_bolalar):
+    ismlar.append(input(f"{n+1}- bolani ismini kiriting:"))
+    print(ismlar)
