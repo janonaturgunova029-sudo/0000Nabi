4c96f5ab6f6685d671e27847f0ff0f69f12752fc
Liliiiin <janonaturgunova029@gmail.com>
2026-04-21 20:18:05 +0900

    Add files via upload

---
+++ 10 if_else vazifa py.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 19:15:45 2026
+
+@author: ASUS
+"""
+# 10 if/else vazifa
+
+#cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+#for c in cars:
+   #if c.lower()=='gm':
+    # print(c.upper())
+   #else:
+   #  print(c.title())
+   
+#cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia'] 
+#for m in cars:
+ #  if m!='gm':
+  #  print(m.title())
+#   else:
+#       print(m.upper())
+       
+#login=input("Login kiriting:")
+# if login.lower()=='admin':
+#   print('Hush kelibsiz Admin')      
+#else:
+      # print(f'Xush kelibsiz  {login.title}')
+      
+#x=float(input('birinchi son kiriting!'))
+#y=float(input('Ikkinchi sonni kiriting!'))
+#if x==y:
+  #  print(f'Sonlar teng: {x}={y}')
+
+
+#son=float(input("istalgan sonni kiriting"))
+#if son>0:
+#    print('Musbat son')
+#else:
+    #print('Manfiy son')
+
+#son=int(input('Istalgan sonni kiriting :'))
+#if son>0:
+# print(f'Bu sonning ildizi {son**(1/2)}') 
+#else:
+#    print('Musbat son kiriting!:')
+
+#osmon_jismlar=['quyosh','yulduz','mars','planeta']
+#for n in osmon_jismlar:
+#  if n.lower()=='mars':
+#      print(n.upper())
+#else:
+ #   print(n.title())
+    
+#osmon_jismlar=['yulduz','mars','oy','yupiter']
+#for o in osmon_jismlar:
+  #  if o.lower()!='oy':
+   #    print(o.title())
+    #else:
+    #    print(o.upper())
+
+#kirish=input('Ismingiz nima?\n>.')
+#if kirish.lower=='admin':
+#    print('Xush kelibsiz Admin')
+#else:
+#    print(f'Xush kelibsiz {kirish.title()}')
+
+x=float(input('Birinchi sonni kiriting:'))
+y=float(input('Ikkinchi sonni kiriting:'))
+if x==y: print(f'Sonlar teng {x}={y}')
+
+
+
+
+
+
