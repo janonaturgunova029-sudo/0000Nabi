96efe29d475bffc302a65769e9fac89b2dbe4bf1
Liliiiin <janonaturgunova029@gmail.com>
2026-04-21 17:51:10 +0900

    Add files via upload

---
+++ if else py.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 13:53:16 2026
+
+@author: 
+"""
+
+#if/ else agar-yoki
+# 10 dars tarmoqlanish
+
+#aftolar=['damas','malibu','bmw','spark','nexia']
+#for a in aftolar:
+  #if a== 'bmw':
+  #  print(a.upper())
+#else:
+ #   print(a.title())
+    
+    
+#ranglar=['qizil', 'sariq','oq','pushti']
+#  # if r == 'oq':
+  #  print(r.upper())
+  # else:
+    #  print(r.title())
+    
+#ism='Ali'
+#ism.lower()=='ali'# Lugatda matni bir xil korinishgan olib kelgan maqul
+
+#ism=input('Ismingiz nima?\n>>')
+#if ism.lower()!='ali':
+#    print(f'Uzur, {ism.title()} biz Alini kutayapmiz')
+#else:
+#       print('Salom Ali')
+       
+       
+       
+#kitob=input('Yoqtirgan kitobingiz qaysi?\n>>')
+#if kitob.lower()!= 'o\'tgan kunlar':
+#    print(f'Uzur biz {kitob.lower()} sohibini emas O\'tgan kunlar sohibini kutayabmiz')
+#else:
+ #  print('hush kelibsiz O\'tgan kunlar kitob sohibi')    
+ 
+#javob=float(input("12x4 nechiga teng? :"))
+#if javob!=48:
+#    print('Javob xato')
+#else:
+   # print('Ofarin topdingiz:)')
+
+#yosh=int(input('Yoshingiz nechida?:'))
+#if yosh>=18:
+#    print('Hush kelibsiz')
+#else:
+#  print('Kirishingiz taqiqlanadi!')
+    
+#login=input('Yangi login tanlang!:')
+#if len(login)<=4:
+   # print('4 Harfdan uzunroq harf tanlang!')
+   
+#yil=int(input("Tug\'ilgan yilingizni kiriting!:"))
+#if 2026-yil<18:
+#    print(f'Yoshingiz {2026-yil} ga teng ekan')
+ #   print('sizga kirish taqiqlanadi!')
+#else:
+   # print('Xush kelibsiz!')
+   
+   
+#yosh=int(input('Yoshingizni kiriting!:'))
+#if yosh>65: print('Siz COVID-19 guruhida ekasniz') # kod qisqa bolsa yonidan bajariladigan kod bilan bajariladigan shartni qoshib yozsa boladi
+
+x,y=71,61
+print(x>y) if x>y else print(x<y)
