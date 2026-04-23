395238b4f21c88ed7eee0120259c41cdabe1379b
Liliiiin <janonaturgunova029@gmail.com>
2026-04-23 17:08:54 +0900

    Add files via upload

---
+++ shartlarni_tekshirish_11 py.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 14:56:48 2026
+
+@author: ASUS
+"""
+
+# 12 hatolat bn ihslash 23.04.26 year
+# paytonda xatolar 3 xil boladi 
+# sintex xatolar
+#indentation erorr bosh joy qolib ketishi qator tashlab ketish
+# run time erorr
+#son=input('Istalgan son kiriting:')
+#son=int(son)
+#print(f'{son} ning kvadrati {son**2} ga teng')
+# type erorr matnda qaysidir qismini tashlab ketganda sodir boladi
+# name erorr ozgaruvchi yolki funksiya nomini xato yozib qoyishdan sodir boladi
+# Valu erorr qiymatni notori yozganda sodir boladi
+#int qoldiqsiz onlik songina hisoblasa floatda butun son ham qoldiq son ham coding yozsa boladi
+#index erorr- royxatni ichidagi elemontga notori nom bn murojat qilish
+#
+#mevalar=['olma','orik','gilos']
+#print(mevalar[2])
+# zero divison erorr Nolga bolish xatoligi
+# Mantiqiy hatolar ozimiz qilgan xatolar
+#radiusi=5
+#pi=4.14 # Hatolik pini qiymatida 3.145
+#aylana_yuzi=radiusi**pi
+#print(aylana_yuzi)
+
+#son=float(input('Istalgan son kiriting'))
+#ildiz=son**0.5 # or-(1/2)
+#print(f'{son} ning ildizi {ildiz} ga ateng')
+
+
+#son=float(input('Juft son kiriting:'))
+#if not son%2==0:
+#    print('Bu son juft emas')
+#else:
+#    print('raxmat!')
+
+#if yosh<=4 or yosh>60:
+#    narh=0
+#elif yosh<18:
+#    narh=1000
+#else:
+#     narh=20000
+    
+#print(f'Chipta barxi {narh} ga teng!')
+
+#x= float(input('Birinchi sonni kiriting:'))
+#y=float(input('Ikkinchi sonni kiritijng:'))
+#if x==y:
+#    print(f'{x}={y}')
+#elif x<y:
+#    print(f'{x}<{y}')
+#else:
+#    print(f'{x}>{y}')
+
+#mahsulotlar=['olma', 'bodom', 'taroq','kartoshka','sarimsoq piyoz']
+#savat=[]
+#for n in range(4):
+#    savat.append(input(f' Savatga {n+1} mahsulotni qoshing:'))
+    
+#for mahsulot in savat:
+#    if mahsulot in mahsulotlar:
+#        print( f'Dokonimizda {mahsulot} bor ')
+  #  else:
+  #      print(f'Dokonimizda {mahsulot} yoq')
+
+#mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+ #              'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+
+#savat = []
+#for n in range(2):
+#    savat.append(input(f'Savatga {n+1}-mahsulotni qo\'shing'))
+
+##mavjud_emas = []
+#for mahsulot in savat:
+#    if mahsulot in mahsulotlar:
+#        bor_mahsulotlar.append(mahsulot)
+#    else:
+#        mavjud_emas.append(mahsulot)
+
+#if mavjud_emas:
+ # print("Do'konimizda quyidagi mahsulotlar yo'q:")
+ # for mahsulot in mavjud_emas:
+ #  print(mahsulot)
+#else:
+#  print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+
+users = ['alisher1983','aziza','yasina' 'umar']
+
+
+login = input("Yangi login tanlang:" )
+if login in users:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
