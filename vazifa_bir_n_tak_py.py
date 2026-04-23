9d280a70596ca19bb341958118272f413b71cb4b
Liliiiin <janonaturgunova029@gmail.com>
2026-04-23 14:55:35 +0900

    Add files via upload

---
+++ vazifa_bir_n_tak_py.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 21:15:33 2026
+
+@author: ASUS
+"""
+# if\elife\alse vazifa 
+
+#son= float(input(' juft son kiriting:'))
+#if son%2:
+#    print('Bu juft son emas')
+#else:
+#    print('thank you')
+
+
+#yosh=float(input('Yoshingizni nechada?\n>>'))
+#if yosh<=4 or yosh>=60:
+#    narh=0
+#elif yosh<18:
+#    narh=10000
+#else:
+#    narh=20000
+#print(f"chipta {narh} so'm")
+
+#3
+#x=int(input('birinchi sonni kiriting:'))
+#y=int(input('Ikkinchi sonni kiriting:'))
+#if x==y:
+#    print(f'{x}={y}')
+#elif x<y:
+#    print(f'{x}<{y}')
+#else:
+#    print(f'{x}>{y}')    
+
+#4
+#mahsulotlar=['olma', 'un','karam','orik', 'bodom','tarvuz','rediska','makaron']
+#savat=[]
+#for n in range(5):
+#  savat.append(input(f'Savatga {n+1} - mahsulotni kiriting?'))
+ 
+               
+#for mahsulot in savat:
+#  if mahsulot in mahsulotlar:
+#        print(f'Dokonimizda {mahsulot}  bor')
+#else:
+#    print(f'Dokonimizda {mahsulot}  yoq!')
+
+#for n in range(5):
+#    savat.append(input(f'Savatga {n+1} mahsulotni qoshing:'))
+ 
+    
+#for mahsulot in savat:
+#    if mahsulot in mahsulotlar:
+#        print(f'Dokonimizda {mahsulot} bor')
+#else:
+#   print(f'Dokonimizda {mahsulot} yoq!')
+
+#son=float(input('Butun son kirting'))
+#for n in range(2,11):
+#    if not (son%n):
+#        print(f'{son} soni {n} ga qoldiqsiz bolinadi!')
+        
+# toq son uchun
+son=float(input('Juft son kiriting!:'))
+for n in range(10,32):
+   if not (son%n):
+       print(f'{son} soni {n} ga qoldiqsiz bolinadi!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
