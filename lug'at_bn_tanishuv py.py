d3998e27c84fb99ec40aa736b823be793dbea98f
Liliiiin <janonaturgunova029@gmail.com>
2026-04-24 21:00:18 +0900

    Add files via upload

---
+++ lug'at_bn_tanishuv py.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 15:34:30 2026
+
+@author: ASUS
+"""
+   
+# Kalit soz qiymat juftligi
+
+#car_0={'MODEL':'ferrari','rangi':'qizil'}  # chap tomondagi kalit soz ikki nuhtaadan keyingisi
+# kalit soz qimati
+#print(car_0['MODEL']) # codni chiqarish uchun ozgaruchi yonida qavsda kalit sozni kirgizsa lugat chiqarkan
+#print(car_0['rangi']) 
+
+#
+#print(k_til['olma'])
+#print(k_til['meva'])
+
+#mevalar={'olma':10000, 'tarvuz':23000, 'orik':3000}
+#print(f'Olmaning narhi {mevalar['olma']} som')
+#print(f'Orikning narhi {mevalar['orik']} so\'m')
+
+#alaba_0={'ism':'murod olimov', 'yosh':26, 't_yil':2000}
+#print(f'{talaba_0['ism'].title()},\
+#      {talaba_0['t_yil']}-yilda tug\'ilgan,\
+#          {talaba_0['yosh']} yoshda') # yangi kalit qoshish uchun
+
+#Yangi kalit sozla kiritish lugat ohiriga qoshiladi
+#talaba_0['kurs']=4
+#talaba_0['yonalish']='informatika'
+#talaba_0['ism']='abdulloh' # Lufatdagi biror qisimini ozgartirush uchun
+
+# bosh lugat yaratish
+
+#talaba_1={} 
+#talaba_1['ism']='lola'
+#talaba_1['kurs']=2
+#talaba_1['yosh']=25
+#print(talaba_1)
+
+#talaba_1['kurs']=3
+#print(f"Talaba {talaba_1['ism'].title()}  {talaba_1['kurs']} kurs va {talaba_1['yosh']} yoshda")
+
+
+# endi lugatdan eleen ochirih
+#del talaba_1['yosh']  # del bilna ochirib yuborish mumkin ozgaruvchi nomi va kalit sozni yozib ochiris
+#print(talaba_1)
+
+#telefonlar={   # KAtta qavs ochib har bir elementni pastdan yozayapmiz lugatni
+ #   'ali':'ipxone x',
+ #   'vali':'galaxy',
+ #   'shavkat':'min 10 pro',
+ #   'tohir':'12 apple'
+#    }
+# .get() metodi keyErorr  hatoligini oldini olishdqa ishlatiladi.
+#phone=telefonlar['ali']
+#print(f'Alining telefoni {phone}')
+
+#phone=telefonlar.get('hasan', 'Bunday ism mavjud emas')
+#print(phone)
+
+##lugat=k_til.get('meva','Bunday soz mavjud emas') # Bunda agar kir#itilga lugat royxatda bolsa lugatni chiqaradi
+# agar kitritlgan lugat bolmasa mavjud emasligini aniqlab beradi.
+#print(lugat)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
