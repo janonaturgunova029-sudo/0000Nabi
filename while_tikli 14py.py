3cbfc049517768db9670fea4ecc50ffd3bcc83d8
Liliiiin <janonaturgunova029@gmail.com>
2026-04-27 19:50:17 +0900

    Add files via upload

---
+++ while_tikli 14py.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 18:15:14 2026
+
+@author: ASUS
+"""
+#WHile tsikli 17 dars
+#   INPUT VA WHILE tsikli
+#ism=input('ismingiz nima:')
+#savol=f'Salom, {ism.title()}. Yoshiningiz nechida?' # Matnni ichida savol ham berilayapti
+#yosh=input(savol)
+#yosh=int(yosh)
+#boy=input('Boyingiz necha sm?')
+#boy=float(boy)
+
+#- While tsikli toki deb tarjima qilib va cod foydalanuchi qanatlangincha toxtamasdan davom etaveradi
+#son=10
+#while son<=15: # % gacha sanab bolganddan keyin tamom bolarkan
+#    print(son, end=' ')
+#    son+=1 #son=son+1 shuni qisqa koninishi
+
+#print('Dastur tugadi')
+
+#print('Kiritilgan sonning kvadratini qaytaruvchi dastur:')
+#savol='Istalgan sonni kiritiing:'
+#savol+="(Dasturni toxtatish uchun 'exit' deb yozing):" # birinchi savolga ikkinchisiini += orqali qoshib olayapti
+#qiymat= ' '
+#while qiymat != 'exit':
+#    qiymat=input(savol)
+#    if qiymat!='exit':
+#       print(float(qiymat)**2)
+
+#print('Dastur tugadi')
+
+
+#
+#print('Kritiligan sonning kubini qaytaruvchi dastur')
+#savol='Istalgan sonni kiriting:'
+#savol += "(dastur to'xtahi uchun 'exit' deb yozing):"
+#qiymat= ' '
+#while qiymat!= 'exit':
+#    qiymat=input(savol)
+#    if qiymat != 'exit':
+#        print(float(qiymat)**3)
+        
+#print('Dastur tugadi')        
+     
+# ISHORA OZGARUVCHI NIMA? bir shart emas turli shartlarni kopini tekshirish ucun ishora ozgaruvchi yaratib olinadi
+#ishora=True
+#while ishora:
+#    qiymat=input(savol)
+#    if qiymat=='exit':
+ #       ishora=False
+#    else:
+#           print(float(qiymat)**3)
+#           
+#print('Dastur to\'xtadi')    
+
+# - BREAK METODI ORQALI DASTURNI TO'XTATISH USULUI
+#while True:
+#    qiymat=input(savol)
+#    if qiymat=='exit':
+#        break
+#    else:
+#        print(float(qiymat)**3)
+        
+#print('Dastur to\'xtatildi!')
+
+# - BREAK APERATORIDAN IF DA HAM ISHLATSA BOLADI
+#sonlar=list(range(1,11))
+#for son in sonlar:
+#    if son==5: # BU YERDA SONNI 5 GACHACHA QILIB TOXTAT DEYAPTI
+#        continue # BU BREAKNI AKSI BUNDA 5 NI TASHLAB KETILDI#break
+#    print(f'{son} ning kvadrati {son**2} ga teng')
+
+#son=0
+#while son<10:
+#    son += 1
+#    if son%2!=0:# != BOLMASA DEGAN MANONI ANGLATADI %2- BU ESA QOLDIQNI TEKSHIRISHDA ISHLATILADIGAN METOD
+#        continue
+#    else:
+#        print(son)
+
+
+# TSKIL BN ISHLAGANDA ABADIY TOXTAMAYDIGAN TSIKL YARATISHDAN SAQLANISH KERAK
+#BUNDAY HATOLARGA MANTIQIY HATOLAR SABAB SODI BOLADI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
