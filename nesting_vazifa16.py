73c26e9d7a33764753126cf1daa2ead7863f37d2
Liliiiin <janonaturgunova029@gmail.com>
2026-04-29 18:34:35 +0900

    Add files via upload

---
+++ nesting_vazifa16.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 19:51:12 2026
+
+@author: ASUS
+"""
+
+#hamkasblar = {
+#    'ali':{'familiya':'valiyev',
+#           'tyil':1995,
+#           'malumot':'oliy',
+#           'tillar':['python','c++']
+#           },
+#    'vali':{'familiya':'aliyev',
+#            'tyil':2001,
+#            'malumot':"o'rta-maxsus",
+#            'tillar':['html', 'css', 'js']},
+#    'hasan':{'familiya':'husanov',
+##             'malumot':'maxsus',
+ #            'tillar':['python','php']}
+#    }
+
+#for ism, info in hamkasblar.items():
+#    print(f"\n{ism.title()} {info['familiya'].title()}, "
+#          f"{info['tyil']}-yilda tug'ilgan. "
+#          f"Ma'lumoti: {info['malumot']}. \n"
+#          "Quyidagi dasturlash tillarini biladi:")
+#    for til in info['tillar']:
+#        print(til.upper())
+
+
+#buxoriy={ 'ism':'Abu Abdulloh Muhhamad ibn Ismoil',
+#    'tyil':810,
+#    'vyil':870,
+#    'tjoy':'Buxoro',
+#    'asarlar':['Al-jome' 'al-sahih ' 'al adab al mufra']
+#    }
+#qodiriy={'ism':'Abdulla Qodiriy',
+#    'tyil':1894,
+#    'vyil':1938,
+#    'tjoy':'Toshkent',
+#    'asarlar':['O\'tkan kunlar' 'Mexrobdan chayon' 'Ozbegim']
+#    }
+#vohidov={'ism':'Erkin Vohidov',
+#         'tyil':1936,
+#         'vyil':2016,
+#         'tjoy':'Farg\'ona',
+#        'asarlar':['Tong nafasi' 'qoshiqlarim sizga']
+#        }
+#navoiy={ 'ism':'Alisher Navoiy',
+#        'tyil':1441,
+#        'vyil':1501,
+#        'tjoy':'Xirot',
+#        'asarlar':['xamsa', 'Lison ut tayr', 'Mahbub-al Qulub']
+#        }
+
+#shaxslar=[buxoriy, qodiriy, vohidov, navoiy]
+
+#for shaxs in shaxslar:
+#    ism=shaxs['ism']
+#    asarlar=shaxs['asarlar']
+#    tyil=shaxs['tyil']
+#    vyil=shaxs['vyil']
+#    tjoy=shaxs['tjoy']
+#    print(f'{ism}{tyil}-yilda {tjoy}da tavvalud topgan',
+#          f'{vyil-tyil} yil mobaynida umr korgan Va shu umrri mobaynida',
+#          f'{asarlar} kabi asarlani yozib qoldirgan')
+#print(f'{ism.upper()} ning mashhur asarlari:')
+#for asar in asarlar:
+#     print(asar.title())   
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
