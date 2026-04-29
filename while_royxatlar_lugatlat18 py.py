dc4b4821d811ecd967bc3ec348c5670566d79fb3
Liliiiin <janonaturgunova029@gmail.com>
2026-04-29 20:29:02 +0900

    Add files via upload

---
+++ while_royxatlar_lugatlat18 py.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 19:20:13 2026
+
+@author: ASUS
+"""
+
+# Royxatlar
+
+#print('Yaqin dostlringixz royxatini tuzamiz:')
+#ismlar=[ ]
+#n=1
+#while True:
+#    savol=f'{n}-dostingizni isini kiriting:'
+#    ism=input(savol)
+#    ismlar.append(ism)
+#    takrorlash=input('Yana ism qoshasizmi? (ha\yo\'q)')
+#    n+=1
+ #   if takrorlash!= 'ha':
+#        break
+
+#print('Do\'stlaringiz ism ro\'yxati!')
+#for ism in ismlar:
+#    print("Do\'tlaringiz ro\'yxati")
+#    print(f'Dostlaringizni ism royxati {ism.title()}')
+
+
+# WHILE TSIKLI BILAN LUGATNI TOLDIRISH
+
+#print('Dostlaringiz yoshini aniqlaymiz:')
+#dostlar={}
+#ishora= True
+#while ishora:
+#    ism=input('Dostingiz ismini kirting:')
+#    yosh=input(f'{ism.title()}ning yoshini kiriting:')
+#    dostlar[ism]=int(yosh)
+    
+#    javob=input('Yana malumot qoshasizmi? (ha\yoq)')
+#    if javob=='yoq':
+#        ishora=False
+        
+#for ism,yosh in dostlar.items():
+#    print(f'{ism.title()} {yosh} yoshda')
+
+
+#WHILE TSIKLI BILAN ROYHATDAGI BIROR QIYMATNI OCHIRISH MUMKIN
+
+#cars=['lasetti','nexia','damas', 'nexia','ferrari', 'nexia']
+#while 'nexia' in cars: # BU WHILE THSIKL BN ROYXAT ICHIDGI HAMMA BIR XIL ELEMENT OCHADI BITTASI EMAS HAMMASI 
+#    cars.remove('nexia')
+#    
+#print(cars)    
+
+#talabalar=['olim','nargiza','abror', 'shuhrat']
+#baholangan_talabalar={}
+#while talabalar:
+#    talaba=talabalar.pop()
+#    baho=input(f'{talaba.title()} ning bahosini kiriting:')
+#    print(f'{talaba.title()} baholandi.')
+#    baholangan_talabalar[talaba]=int(baho)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
