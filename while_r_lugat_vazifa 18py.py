917ad359c444e4ce5b421b6ceba0f2afbe8369df
Liliiiin <janonaturgunova029@gmail.com>
2026-04-29 21:21:53 +0900

    Add files via upload

---
+++ while_r_lugat_vazifa 18py.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 20:29:50 2026
+
+@author: ASUS
+"""
+# OZIMYOZGA LEKIN NOTOGRI CODE
+#print('Buyurtma nomini kiriting!:')
+#buyurtmalar=[]
+#n=1
+#while True:
+#    savol=f'{n} buyurtma nomini kiriting:'
+#    buyurtma=input(savol)
+#    savol+='Buyurtma yana qoshasizmi? (ha|yoq)'
+#    n+=1
+#    buyurtmalar.append(buyurtma)
+#    qabul=input(savol)
+#    takrorlash='Buyurtma yana qoshasizmi? (ha|yoq)'   
+#    n+=1
+#    if takrorlash!='ha':
+#        break
+    
+# DARSDAGI TOGRI KODE
+#savat=[]
+#while True:
+#    mahsulot=input('Savatga mahsulot qoshing:')
+#    savat.append(mahsulot)
+#    javob=input('Yana mahsulot qoshasizmi? (ha\yo\'q)')
+#    if javob!= 'ha':
+# #       break
+    
+
+#ranglar=[]
+#while True:
+#    rang=input('Yoqtirgan rangingizni kiriting-')
+#    ranglar.append(rang)
+#    qoshish=input('Yana rang qoshasizmi? (ha\yoq)')
+#    if qoshish!= 'ha':
+#        break
+#print('Sizning yoqtirgan ranglaringiz quyidagilar;')
+#print(ranglar)
+
+#buyurtmalar=['olma','orik','shaftoli', 'anjir']
+#mahsulotlar={
+#    'olma':3000,
+#    'orik':2000,
+ #   'anjir':4000,
+#    'shaftoli':2000
+#    }
+#while buyurtmalar:
+#    buyurtma=buyurtmalar.pop()
+#    if buyurtma in mahsulotlar.keys():
+#        narh=mahsulotlar[buyurtma]
+#        print(f'{buyurtma.title()} - {narh} som')
+#    else:
+#       print(f'Bizda {buyurtma} yoq!')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
