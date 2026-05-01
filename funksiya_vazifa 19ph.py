b214eda72a0ad9dd8590f778ff038890f54b1018
Liliiiin <janonaturgunova029@gmail.com>
2026-05-01 21:08:50 +0900

    Add files via upload

---
+++ funksiya_vazifa 19ph.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 19:09:10 2026
+
+@author: ASUS
+"""
+
+#def yoshini_sora(ism,yosh):
+#    """Foydalanuvchini ismi va yoshini soraydigan funksiya"""
+#    print(f'Foydalanuvchining ismi {ism.title()}, yoshi esa {yosh} yoshda')
+    
+    
+#yoshini_sora('anvar',24)
+    
+    
+#def son_kvadrati_kubini_hisobla(son):
+#    """Sonning kvadrati va kubini hisoblovchi funksiya """
+#     print(f'{son} ning kvadrati {son**2} ga teng uning kubi esa {son**3} ga teng')
+     
+#son_kvadrati_kubini_hisobla(3)
+    
+#def son_juft_toqligini_hisobla(son):
+#    """Sonning juft yoki toqligini isoblovchi funksiya"""
+#    if son%2==0:
+#        print(f'{son} soni juft son')
+#    else:
+#        print(f'{son} toq son')
+        
+#son_juft_toqligini_hisobla(6)
+ # 4   
+#def son_katta_kichikligini_aniqla(x,y):
+#      """Sonning katta yoki kichikligini aniqlash funksiyasi"""
+#      if x>y:
+#          print(f'{x}>{y}')
+#      elif x<y:
+#          print(f'{x}<{y}')
+#      else:
+#          print(f'{x}={y}')
+          
+#son_katta_kichikligini_aniqla(8,7)        
+#son_katta_kichikligini_aniqla(-9,4)
+#son_katta_kichikligini_aniqla(12,-90)
+ 
+#5 savol
+#def s_darajasi(x,y=2):
+#    "sonninng darajasini aniqlash funksiyasi"
+#    print(f'{x} soninging {y}-darajasi {x**y} ga teng ')
+    
+#s_darajasi(9)    
+#s_darajasi(2,4)
+#s_darajasi(3,6)
+#s_darajasi(11,17)    
+
+
+   
+   ##7
+   
+    
+def qoldiqsiz_bol(son):
+    for n in range(2,11):
+        if not son%n:
+            print(f"{son} {n} ga qoldiqsiz bolinadi!")
+    
+qoldiqsiz_bol(12)#Python’da 0 qiymati False hisoblanadi, boshqa har qanday butun son esa True.
+
+ #not operatori qiymatni teskarisiga o‘giradi:
+
+ #ot 0 → True
+
+ #not (qoldiq ≠ 0) → False
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
