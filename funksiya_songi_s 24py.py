bd0a977f13fa85495df7633f5c51b4cfc998b021
Liliiiin <janonaturgunova029@gmail.com>
2026-05-10 14:45:34 +0900

    Add files via upload

--- funksiya_songi_s 24py.py
+++ funksiya_songi_s 24py.py
@@ -51,13 +51,63 @@ sonlar=list(range(15))
 
 #print(son(math.sqrt))
 
-def daraja2(x): # Istalgan x sonini kvadratini qaytaradi
-        """Berilgan sonni kvadratini qaytaruvchi funksiya"""
-        return x*x
+#def daraja2(x): # Istalgan x sonini kvadratini qaytaradi
+#        """Berilgan sonni kvadratini qaytaruvchi funksiya"""
+#        return x*x
 
-print(list(map(daraja2,sonlar)))
+#print(list(map(daraja2,sonlar)))
+
+#kvadratlar=list(map(lambda x:x*x,sonlar)) # buy yerda lambda funksiyasi orqalei 
+# sonlar yaratilib sonlardagi qiymat x ga yuklanadi.
+
+#from math import sqrt
+
+#sonlar=list(range(13))
+#ildizla=list(map(sqrt,sonlar))
+#print(ildizla)
+
+#sonlar=list(range())
 
 
+#def daraja2(x):
+#    """Berilgan sonning darajasini aniqlovchi funksiya"""
+#    return x*x
+
+
+#sonlar=list(range(1,17))
+#print(list(map(daraja2,sonlar))) # BU YERDA FUNKSIYA YARATIB SHU FUNKSIYADA FOYDALANIB MEP BN SONLARNI BIRLASHTIRAYAPTI
+
+#kvadrat=list(map(lambda x:x*x, sonlar))
+#print(kvadrat)
+
+
+#a=[4,5,8]
+#b=[4,9,2]
+#a_plus_b=list(map(lambda x,y:x+y,a,b))
+#print(a_plus_b)
+
+
+#import random as r  #YANGI FILTER DEB ATALGAN FUNKSIYA 
+#sonlar=r.sample(range(100),10)
+#print(sonlar)
+#def juftmi(x):
+    
+##    return x%2==0 #qoldiqni topish uchun shunday 2 % bolindi
+
+#juft_sonlar= list(filter(juftmi,sonlar)) # HUDDI MAPDEK BIR FUNKSIYA VA QIYMAT QABUL QILIB SAALAB BERADI
+
+#juft_sonlar=list(filter(lambda son: son%2==0,sonlar))
+#print(juft_sonlar)
+
+
+
+mevalar=['olma','anor','orik','gilos','limon','ananas','tarvuz']
+#harf='a'
+#mevalar_a=list(filter(lambda meva:meva.startswith(harf),mevalar))
+#print(mevalar_a)
+
+mevalar2=list(filter(lambda meva:len(meva)<=4,mevalar))
+print(mevalar2)
 
 
 
