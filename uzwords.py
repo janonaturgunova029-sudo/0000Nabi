43f3228a099a0ce379cdd23e69ba6c1c8380bcc5
Liliiiin <janonaturgunova029@gmail.com>
2026-05-13 21:40:10 +0900

    Add files via upload

---
+++ uzwords.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 21:20:12 2026
+
+@author: ASUS
+"""
+
+words=[
+      'alpomish',
+      'mafkura',
+      'tahlil',
+      'samara',
+      'bog-rog',
+      'lola',
+      'shongimoq',
+      'sukunat',
+      'ber',
+      'asil',
+      'nolishga',
+      'maktub',
+      'saviya'
+      ]
