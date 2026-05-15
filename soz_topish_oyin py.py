1e748dd0f58df3f90048bf11b3f170be2ec5ab14
Liliiiin <janonaturgunova029@gmail.com>
2026-05-15 16:16:57 +0900

    Add files via upload

--- soz_topish_oyin py.py
+++ soz_topish_oyin py.py
@@ -1,4 +1,4 @@
-# SOZ TOP OYINI KOD
+ # SOZ TOP OYINI KOD
 import random
 from uzwords import words
 
@@ -19,17 +19,28 @@ def display(user_letters,word): # Bu funksiyasini vazifasi foydalanuvchi topgan 
             display_letter+="-"
     return display_letter
 
-def play():
-    word=get_word()
+def play(): ##words lugatini ichidan tasodifiy sozni olish funksiyasi
+    word=get_word() # Get word degan funsiyani ichiga random choice qilib yuklagan sozni bu yerda word degan ozgarucgia yuklayapti
     word_letters=set(word)  # setdan foydalanishga sabab qaytarilgan sozlar bomasdan chiqarib beradi
-    user_letters=''                        ## Soz ichidagi harflarni ikkita bir xil qatnashgan bolsa birini ochirib faqat bittasini chiqarib berDI
-    print(f'Men {len(word)} xonali son oyladim. Topa olasizmi?')
+    user_letters='' 
+##Bu yerda foydalanuvchi kiritgan sozlarni us-lettni ichiga yuklab boriladi                       ## Soz ichidagi harflarni ikkita bir xil qatnashgan bolsa birini ochirib faqat bittasini chiqarib berDI
+    print(f'Men {len(word)} xonali soz oyladim. Topa olasizmi?')
     while len(word_letters)>0:
         print(display(user_letters,word))
         if len(user_letters)>0:
-            print(f'Shu vaqtgacha kiritgan harflaringiz: {user_letters}')
+           print(f'Shu vaqtgacha kiritgan harflaringiz: {user_letters}')
 
-
+        letter=input("Xarf kiriting:").upper()
+        if letter in user_letters:
+            print('Bu harfni avval kiritgansiz. Boshqa harf kiriting!')
+            continue
+        elif letter in word:
+            word_letters.remove(letter)
+            print(f"{letter} xarf togri.")
+        else:
+        print('Bunday harf yoq.')
+        user_letters+=letter
+    print(f'Tabriklayman! {word} sozini {len(user_letters)} ta urinishda topdingiz!')
 
 
 
