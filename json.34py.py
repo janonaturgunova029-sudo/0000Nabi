2d5664744c1a15c2e69dfad6db67b884db61ba9a
Liliiiin <janonaturgunova029@gmail.com>
2026-06-26 21:22:26 +0900

    Add files via upload

---
+++ json.34py.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 20:23:50 2026
+
+@author: ASUS
+"""
+import json #ozgaruvchilar stringda boladi
+#import googlemaps
+#rom apokey import APIKEY
+
+##GoogleMaps
+x=10
+x_json=json.dumps(x)
+
+y=5.5
+y_json=json.dumps(y)
+
+
+m=True
+m_json=json.dumps(m) ##Bu jsonda matn phtonda yozilsa ham json bilan avval Java skriptga otadi keyin esa phytonga otadi
+
+
+sonlar=(12,13,14,15,16)
+sonlar_json=json.dumps(sonlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
